a5339adb1b4d3c14595aca17826e2b3d0d4d385c
ZaidDeAnda <zaidthejedi@gmail.com>
2025-07-21 10:23:00 -0600

    initial day2

---
+++ pages/Dia_2.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.header("Contenidos del día 2")
+
+cols = st.columns(4)
+
+cols[0].subheader("Notebook para clase de python")
+
+cols[0].markdown("[![Open In Colab](https://colab.research.google.com/assets/colab-badge.svg)](https://colab.research.google.com/github/GabrielMissael/CosmicDistortions/blob/day1/notebooks/Curso_de_Python_D%C3%ADa_1_Clubes_de_ciencia_2025.ipynb)")
