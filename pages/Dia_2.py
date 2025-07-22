b321733a9429fb82c670f857dc2b5c31ab42f18c
ZaidDeAnda <zaidthejedi@gmail.com>
2025-07-22 10:29:15 -0600

    added cv button

--- pages/Dia_2.py
+++ pages/Dia_2.py
@@ -7,3 +7,7 @@ cols = st.columns(4)
 cols[0].subheader("Notebook para clase de python")
 
 cols[0].markdown("[![Open In Colab](https://colab.research.google.com/assets/colab-badge.svg)](https://colab.research.google.com/github/GabrielMissael/CosmicDistortions/blob/day1/notebooks/Curso_de_Python_D%C3%ADa_2_Clubes_de_ciencia_2025_ipynb.ipynb)")
+
+cols[1].subheader("Notebook para computer vision")
+
+cols[1].markdown("[![Open In Colab](https://colab.research.google.com/assets/colab-badge.svg)](https://colab.research.google.com/github/GabrielMissael/CosmicDistortions/blob/day1/notebooks/Visi%C3%B3n_computacional_Clubes_de_ciencia_2025_GTO_2.ipynb)")
