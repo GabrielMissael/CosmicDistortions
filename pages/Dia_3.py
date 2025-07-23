3c7412dd5b86c4e79e07b56f85241211cec65e7c
ZaidDeAnda <zaidthejedi@gmail.com>
2025-07-23 10:31:03 -0600

    added day 3

---
+++ pages/Dia_3.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.header("Contenidos del día 3")
+
+cols = st.columns(4)
+
+cols[0].subheader("Notebook para perceptrones")
+
+cols[0].markdown("[![Open In Colab](https://colab.research.google.com/assets/colab-badge.svg)](https://colab.research.google.com/github/GabrielMissael/CosmicDistortions/blob/day1/notebooks/CdeCMx2025_GT02_Introduccion_redes_neuronales.ipynb)")
+
+cols[1].subheader("Notebook para lentes gravitacionales")
+
+cols[1].markdown("[![Open In Colab](https://colab.research.google.com/assets/colab-badge.svg)](https://colab.research.google.com/github/GabrielMissael/CosmicDistortions/blob/day1/notebooks/CdeCMx2025_GT02_simulando_lentes_gravitacionales.ipynb)")
