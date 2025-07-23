acfe5cfa537de6dd130162c27b5c539bc31661ce
ZaidDeAnda <zaidthejedi@gmail.com>
2025-07-23 10:37:06 -0600

    some configs

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
+st.set_page_config(layout="wide", page_title="GTO2", page_icon="🌠")
+
 st.header("Cosmic distortions - CdeC GTO 2 2025")
 
 st.write("Bienvenidxs! Esta página servirá como un directorio a todos los contenidos del curso.")
