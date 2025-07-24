d7551aebbe84c90cbae31330462b333ccd6c6fe6
Gabriel Missael Barco <missaelgabo@gmail.com>
2025-07-24 12:25:37 -0600

    Añado notebook para lentes con tus propias imágenes

--- pages/Dia_4.py
+++ pages/Dia_4.py
@@ -9,3 +9,7 @@ cols = st.columns(2)
 cols[0].subheader("Notebook para redes neuronales convolucionales")
 
 cols[0].markdown("[![Open In Colab](https://colab.research.google.com/assets/colab-badge.svg)](https://colab.research.google.com/github/GabrielMissael/CosmicDistortions/blob/day1/notebooks/Redes_neuronales_convolucionales_CdeC_2025_GTO2.ipynb)")
+
+cols[1].subheader("Notebook para simular lente con tu propia imágen")
+
+cols[1].markdown("[![Open In Colab](https://colab.research.google.com/assets/colab-badge.svg)](https://colab.research.google.com/github/GabrielMissael/CosmicDistortions/blob/day1/notebooks/Simulando_un_lente.ipynb)")
