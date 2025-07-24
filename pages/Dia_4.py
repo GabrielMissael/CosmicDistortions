154f3212148107b1a6fdf16924af8d6575bd08b5
ZaidDeAnda <zaidthejedi@gmail.com>
2025-07-24 00:26:41 -0600

    added day4 button

--- pages/Dia_4.py
+++ pages/Dia_4.py
@@ -6,5 +6,4 @@ cols = st.columns(2)
 
 cols[0].subheader("Notebook para redes neuronales convolucionales")
 
-cols[0].markdown("[![Open In Colab](https://colab.research.google.com/assets/colab-badge.svg)](https://colab.research.google.com/github/GabrielMissael/CosmicDistortions/blob/day1/notebooks/CdeCMx2025_GT02_Introduccion_redes_neuronales.ipynb)")
-
+cols[0].markdown("[![Open In Colab](https://colab.research.google.com/assets/colab-badge.svg)](https://colab.research.google.com/github/GabrielMissael/CosmicDistortions/blob/day1/notebooks/Redes_neuronales_convolucionales_CdeC_2025_GTO2.ipynb)")
